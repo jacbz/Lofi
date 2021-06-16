6578a2dfe9736d23e3a96c70976849d26475089c
Jacob Zhang <jacob-z@live.de>
2021-06-16 17:17:23 +0200

    Set up client

---
+++ client/README.md
@@ -0,0 +1,13 @@
+# Lofi music client
+
+* Prerequisites: Node.js 14
+* Recommended: Visual Studio Code
+
+## Develop
+
+1. Install dependencies with `npm install`
+2. Serve client with `npm run serve`
+
+## Build
+
+To build a distributable version into the `dist` folder, run `npm run build`
---
+++ client/webpack.common.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/index.ts', './src/style.scss'],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'assets' }]
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  }
+};
