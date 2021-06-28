7e0463a455ecba980ec25a3578723e7c2160d831
muell <muellerseb@cip.ifi.lmu.de>
2021-06-28 09:49:59 +0200

    Labled Loops

--- client/src/tracks/TrackFinder.ts
+++ client/src/tracks/TrackFinder.ts
@@ -3,6 +3,9 @@ import { TrackInput } from "./TrackInput";
 import * as drumFillsTracks from "./files/DrumFills.json";
 import * as drumLoopsTracks from "./files/DrumLoops.json";
 import * as fullDrumLoopsTracks from "./files/FullDrumLoops.json";
+import * as hiHatLoopsTracks from "./files/HiHatLoops.json";
+import * as percussionLoopsTracks from "./files/PercussionLoops.json";
+import * as topLoopsTracks from "./files/TopLoops.json";
 
 export class Trackfinder {
 
@@ -11,6 +14,12 @@ export class Trackfinder {
     drumLoopsTracks: Array<TrackFile> = drumLoopsTracks;
 
     fullDrumLoops: Array<TrackFile> = fullDrumLoopsTracks;
+
+    hiHatLoops: Array<TrackFile> = hiHatLoopsTracks;
+
+    percussionLoops: Array<TrackFile> = percussionLoopsTracks;
+
+    toploops: Array<TrackFile> = topLoopsTracks
 
     searchTracks(trackInput: TrackInput): Array<TrackFile> {
         const trackFiles = new Array<TrackFile>();
